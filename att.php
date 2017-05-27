f67389fae3957f5bc7f29809abfb97ad948a358d
Matt Jenkins <matt@majenko.co.uk>
2017-05-27 21:02:29 +0100

    Added download count

--- att.php
+++ att.php
@@ -7,6 +7,7 @@ $q = db_query("select * from attachments where id=:id", array("id" => $id));
 if ($r = db_next($q)) {
     header("Content-type: " . $r->mime);
     print $r->data;
+    db_update("attachments", $r->id, array("downloads" => $r->downloads + 1));
 } else {
     header("HTTP/1.0 400 Bad Request");
 }
